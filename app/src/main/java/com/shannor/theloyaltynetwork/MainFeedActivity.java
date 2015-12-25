63cc5f88d2e24201fe119f19a5a1f1d41f5990bd
Shannor Trotty <Shannor.a.trotty@gmail.com>
2015-12-24 23:13:04 -0500

    Changed MainFeed to a Viewpager and added one Fragment More to come in the future

--- app/src/main/java/com/shannor/theloyaltynetwork/MainFeedActivity.java
+++ app/src/main/java/com/shannor/theloyaltynetwork/MainFeedActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -11,10 +13,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewParent;
 
 import com.shannor.theloyaltynetwork.mangers.PostManager;
 import com.shannor.theloyaltynetwork.model.Post;
 import com.shannor.theloyaltynetwork.model.User;
+import com.shannor.theloyaltynetwork.views.MainActivityFragmentAdapter;
 import com.shannor.theloyaltynetwork.views.PostAdapter;
 
 import java.util.ArrayList;
@@ -23,18 +27,22 @@ import java.util.List;
 public class MainFeedActivity extends AppCompatActivity {
 
     List<User> userList = new ArrayList<>();
-    PostManager postManager = PostManager.getInstance();
-    RecyclerView mRecyclerView;
-    PostAdapter mPostAdapter;
+    ViewPager mViewPager;
+    TabLayout mTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_feed);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mViewPager = (ViewPager)findViewById(R.id.viewpager);
+        mTabLayout = (TabLayout)findViewById(R.id.fragment_tabs);
 
-        //TODO:Added a view pager and tabs for multiple pages
+        MainActivityFragmentAdapter mainActivityFragmentAdapter = new MainActivityFragmentAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(mainActivityFragmentAdapter);
+        mTabLayout.setupWithViewPager(mViewPager);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -43,21 +51,6 @@ public class MainFeedActivity extends AppCompatActivity {
                 initPost();
             }
         });
-
-        userList.add(new User("Shannor"));
-        userList.add(new User("Father"));
-        userList.add(new User("Mother"));
-
-        postManager.addPost(userList.get(0),"Test Post","Body!!!!");
-        postManager.addPost(userList.get(1),"Test Post Number Two","Body!!!!");
-        postManager.addPost(userList.get(2),"Test Post Number Three","Body!!!!");
-
-        mRecyclerView = (RecyclerView)findViewById(R.id.mainFeed);
-        mRecyclerView.setHasFixedSize(true);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        mPostAdapter = new PostAdapter(postManager.getPostList());
-        mRecyclerView.setAdapter(mPostAdapter);
-
     }
 
     @Override
---
+++ app/src/main/java/com/shannor/theloyaltynetwork/views/MainActivityFragmentAdapter.java
@@ -0,0 +1,41 @@
+package com.shannor.theloyaltynetwork.views;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.shannor.theloyaltynetwork.fragments.MainFeedFragment;
+
+/**
+ * Created by Shannor on 12/24/2015.
+ * Fragment adapter used for all the fragments in the mainFeed
+ */
+public class MainActivityFragmentAdapter extends FragmentPagerAdapter {
+
+        String[] fragmentTitles = {
+                "Feed"
+        };
+
+        public MainActivityFragmentAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new MainFeedFragment();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentTitles.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragmentTitles[position];
+        }
+}
